c4bd28bcb8ce4fd3e26f8d9a17c8ef3c8e63bd35
Manaligorajiya <103837914+Manaligorajiya@users.noreply.github.com>
2022-05-21 11:19:09 +0530

    Add files via upload

---
+++ TASK3/script.js
@@ -0,0 +1,81 @@
+var selectedRow = null
+
+function Submit(e) {
+	event.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
+
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData["name"] = document.getElementById("name").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["en"] = document.getElementById("en").value;
+    formData["sem"] = document.getElementById("sem").value;
+    formData["cname"] = document.getElementById("cname").value;
+
+    return formData;
+}
+
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.name;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.email;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.en;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.sem;
+    cell5 = newRow.insertCell(4);
+		cell5.innerHTML = data.cname;
+    cell5 = newRow.insertCell(5);
+        cell5.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
+
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("en").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("sem").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("cname").value = selectedRow.cells[4].innerHTML;
+
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.name;
+    selectedRow.cells[1].innerHTML = formData.email;
+    selectedRow.cells[2].innerHTML = formData.en;
+    selectedRow.cells[3].innerHTML = formData.sem;
+    selectedRow.cells[4].innerHTML = formData.cname;
+
+}
+
+//Delete the data
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+//Reset the data
+function resetForm() {
+    document.getElementById("name").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("en").value = '';
+    document.getElementById("sem").value = '';
+    document.getElementById("cname").value = '';
+    selectedRow = null;
+}
